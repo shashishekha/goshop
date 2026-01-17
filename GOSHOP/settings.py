8652702e62b67be17b422e002efc91c784cbae06
Shashi Shekhar <shashiyadav7629518@gmail.com>
2026-01-17 17:19:00 +0530

    postgres db connection made. Bootstrap frontend setup done

--- GOSHOP/settings.py
+++ GOSHOP/settings.py
@@ -78,7 +78,12 @@ WSGI_APPLICATION = 'GOSHOP.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'ecomm',
+        'USER': 'postgres',
+        'PASSWORD':'root',
+        'HOST':'localhost',
+        'PORT':'5432',
     }
 }
 
@@ -117,7 +122,19 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+import os
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+
+STATICFILES_DIR={
+    os.path.join(BASE_DIR, "public/static" )
+}
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'public/static')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,7 @@
 
 from django.urls import path
+from .views import *
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', home),
 ]
